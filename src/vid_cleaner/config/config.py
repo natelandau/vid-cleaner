c93c3540362e91218b0137e730105498d96bb6a6
Nathaniel Landau <natelandau@users.noreply.github.com>
2024-02-21 12:07:03 -0500

    refactor: move version to constants (#22)

--- src/vid_cleaner/config/config.py
+++ src/vid_cleaner/config/config.py
@@ -11,24 +11,24 @@ from vid_cleaner.constants import APP_DIR, CONFIG_PATH
 PATH_CONFIG_DEFAULT = Path(__file__).parent / "default_config.toml"
 
 
-def pass_log_to_file(value: str) -> bool:
-    """Confz does not work well with Typer options. Confz requires a value for each CLI option, but Typer does not. To workaround this, if --log-to-file is passed, we set the value to "True" regardless of what follows the CLI option."""
+def pass_opt_without_value(value: str) -> bool:
+    """Confz does not work well with Typer options. Confz requires a value for each CLI option, but Typer does not. To workaround this, for example, if --log-to-file is passed, we set the value to "True" regardless of what follows the CLI option."""
     if value:
         return True
 
     return False
 
 
-LOG_TO_FILE = Annotated[
+OPT_BOOLEAN = Annotated[
     bool,
-    BeforeValidator(pass_log_to_file),
+    BeforeValidator(pass_opt_without_value),
 ]
 
 
 class VidCleanerConfig(BaseConfig):  # type: ignore [misc]
     """Configuration class for vid - cleaner."""
 
-    log_to_file: LOG_TO_FILE = False
+    log_to_file: OPT_BOOLEAN = False
     log_file: Path = APP_DIR / "vid_cleaner.log"
     keep_languages: frozenset[str] = frozenset(["eng"])
     radarr_api_key: str = ""
