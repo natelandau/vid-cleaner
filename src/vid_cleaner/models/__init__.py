333f5d80a298807a32f738788338a7faf9d3001f
Nathaniel Landau <github@natelandau.com>
2026-07-23 09:31:13 -0400

    feat(clean): add PlanAction records to the conversion plan

--- src/vid_cleaner/models/__init__.py
+++ src/vid_cleaner/models/__init__.py
@@ -1,6 +1,6 @@
 """Models for vid-cleaner app."""
 
-from .conversion_plan import ConversionPlan, OutputStream
+from .conversion_plan import ConversionPlan, OutputStream, PlanAction
 from .video_file import VideoFile
 
-__all__ = ["ConversionPlan", "OutputStream", "VideoFile"]
+__all__ = ["ConversionPlan", "OutputStream", "PlanAction", "VideoFile"]
